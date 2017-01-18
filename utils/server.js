eb15bc3c1cdacaaff1ba6f72271b6f9d45f4d5e0
Paul Cothenet <paul@madkudu.com>
2017-01-17 18:32:41 -0800

    Fix linting issues

--- utils/server.js
+++ utils/server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+const PORT = 3004;
 
 app.use(express.static('dist'));
 
-app.listen(3004, function () {
+app.listen(PORT, function () {
 	console.log('Listening on port 3004');
 });
-
