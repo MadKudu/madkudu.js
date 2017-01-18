615e67bff60e560fe139466b0af1e240269edf25
Paul Cothenet <paul@madkudu.com>
2017-01-18 15:02:34 -0800

    Improve predict call in user

--- utils/run_compiler.js
+++ utils/run_compiler.js
@@ -3,13 +3,14 @@ const watch = process.env.WATCH === 'true';
 
 const Compiler = require('./compiler');
 
-const DEFAULT_OPTIONS = { min: true };
+const DEFAULT_SETTINGS = require('./default_settings');
+const DEFAULT_OPTIONS = {};
 
-const compiler = new Compiler();
+const compiler = new Compiler(DEFAULT_SETTINGS, DEFAULT_OPTIONS);
 
 if (watch) {
 	compiler.watch();
 } else {
-	compiler.run(DEFAULT_OPTIONS)
+	compiler.run({ min: true })
 		.then(() => process.exit(0));
 }
