8eaec1342b30042a573150409e2b175f10f9ac96
Paul Cothenet <paul@madkudu.com>
2020-03-19 17:50:08 -0700

    Re-enable warning for Sauce

--- utils/get_webpack_config.js
+++ utils/get_webpack_config.js
@@ -18,11 +18,11 @@ module.exports = function (settings = {}, options = {}) {
     webpack_config.output = {
       libraryTarget: 'var'
     }
-    // if (!process.env.SAUCE_USERNAME) {
-    //   throw new Error('A SAUCE_USERNAME is required')
-    // } else if (!process.env.SAUCE_ACCESS_KEY) {
-    //   throw new Error('A SAUCE_ACCESS_KEY is required')
-    if (!process.env.SEGMENT_API_KEY) {
+    if (!process.env.SAUCE_USERNAME) {
+      throw new Error('A SAUCE_USERNAME is required')
+    } else if (!process.env.SAUCE_ACCESS_KEY) {
+      throw new Error('A SAUCE_ACCESS_KEY is required')
+    } else if (!process.env.SEGMENT_API_KEY) {
       throw new Error('A SEGMENT_API_KEY is required')
     } else if (!process.env.MADKUDU_API_KEY) {
       throw new Error('A MADKUDU_API_KEY is required')
